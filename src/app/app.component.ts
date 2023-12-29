3884dd3f0b0c7e7efdf255ddd35e439b6d2ecc8b
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2023-12-29 14:59:51 -0500

    Home, navbar, and about me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {NavComponent} from "./components/nav/nav.component";
+import { HomeComponent } from "./components/home/home.component";
+import { SobremiComponent } from "./components/sobremi/sobremi.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, NavComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, NavComponent, HomeComponent, SobremiComponent]
 })
 export class AppComponent {
-  title = 'portfolio';
 }
