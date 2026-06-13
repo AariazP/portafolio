b4d3f611e6eb87eb3f509c42a602404431189c5a
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2026-06-12 22:05:43 -0500

    update: add new design

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,7 +9,7 @@ import { ScrollRevealDirective } from '../../directives/scroll-reveal.directive'
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
-  correo = "alejo17ariazpzn@gmail.com";
+  correo = "aariazp@gmail.com";
 
   socials = [
     { name: 'LinkedIn', url: 'https://www.linkedin.com/in/jos%C3%A9-alejandro-arias-pinz%C3%B3n-20253a24a/', icon: 'bi bi-linkedin' },
